6eb9d3d6fe5b11fcb09eaeea9b20788537a7580d
Akshay Bodla <akshaybodla21@gmail.com>
2020-12-23 10:14:02 -0800

    Vehicle rotation and full player movement with old input system

--- Prototype-1/Assets/Course Library/Scripts/PlayerController.cs
+++ Prototype-1/Assets/Course Library/Scripts/PlayerController.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] float vSpeed;
-    void Start()
-    {
+    [SerializeField] float vehicleSpeed, turnSpeed;
+    [SerializeField] float horizontalInput, forwardInput;
+
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(Vector3.forward * vSpeed * Time.deltaTime);
+    void Update() {
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput   = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * vehicleSpeed * forwardInput * Time.deltaTime);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        // transform.Translate(Vector3.right * turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
