cf7c89fb0528fd7037b869e6ed34be84ea773393
Akshay Bodla <akshaybodla21@gmail.com>
2020-12-23 09:50:21 -0800

    Camera script to follow player's vehicle

---
+++ Prototype-1/Assets/Course Library/Scripts/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject vehicle;
+    [SerializeField] private Vector3 camOffset;
+    // [SerializeField] private float camOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.position = new Vector3(vehicle.transform.position.x,7,vehicle.transform.position.z + camOffset);
+
+        transform.position = vehicle.transform.position + camOffset;
+    }
+}
